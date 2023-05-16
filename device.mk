d44e0c2633e743d3b3c14e8848645b25da459118
BladeRunner-A2C <prisoner56898@protonmail.com>
2023-05-16 21:50:42 +0600

    redwood: Don't use MIUI camera for now
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>

--- device.mk
+++ device.mk
@@ -73,8 +73,6 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.qcom.bluetooth.twsp_state.enabled=false
 
 # Camera
-$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
-
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
